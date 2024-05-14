b977057a9a7f96a6aa0802b11f11142137be68b5
Yang Li <liyang9249@gmail.com>
2024-05-13 22:05:50 -0700

    reformat and add setuptools to requirements-dev.txt

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='openapi-validator',
-    version='0.0.1',
+    name="openapi-validator",
+    version="0.0.1",
     packages=find_packages(),
-    license='Your License',
-    author='Yang Li',
-    description='Short description of your project',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    install_requires=[
-        "requests==2.31.0", "openapi-core==0.19.1"
-    ],
+    license="Your License",
+    author="Yang Li",
+    description="Short description of your project",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=["requests==2.31.0", "openapi-core==0.19.1"],
     tests_require=[
         "pytest==8.2.0",
-    ]
+    ],
 )
