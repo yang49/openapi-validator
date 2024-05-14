00f5272bdf1dc6d3b818b50e3e608364aab70e50
Yang Li <liyang9249@gmail.com>
2024-05-13 21:51:28 -0700

    Add more unit tests

--- tests/unit/test_openapi_validator.py
+++ tests/unit/test_openapi_validator.py
@@ -1,15 +1,27 @@
 import unittest
 from pathlib import Path
 
+## Add to the home path
 import sys
-print(sys.path)
+
+file_path = str(Path(__file__).parent.parent.parent)
+sys.path.append(file_path)
 
 from src.openapi_validator import OpenApiValidator
 
+
 class TestOpenAPIValidator(unittest.TestCase):
 
-    def test_validate_request(self):
-        path = Path(__file__).with_name('openapi.yaml')
+    def test_create_validator_with_valid_path(self):
+        path = Path(__file__).with_name("openapi.yaml")
         validator = OpenApiValidator(str(path))
         self.assertIsNotNone(validator)
-        
+
+    def test_create_validator_with_invalid_path(self):
+        with self.assertRaises(Exception):
+            OpenApiValidator("random_path")
+
+    def test_create_validator_with_invalid_yaml(self):
+        path = Path(__file__).with_name("invalid_openapi.yaml")
+        with self.assertRaises(Exception):
+            OpenApiValidator(str(path))
